8d8fbe0fa9d801c509cadfabf29cd7247eea1abf
Fi1osof <n.lanets@modxclub.ru>
2020-12-08 02:22:51 +0300

    Added static files declaration

---
+++ src/@types/files.d.ts
@@ -0,0 +1,24 @@
+declare module '*.svg' {
+  const value: string
+  export default value
+}
+
+declare module '*.png' {
+  const value: string
+  export default value
+}
+
+declare module '*.jpg' {
+  const value: string
+  export default value
+}
+
+declare module '*.jpeg' {
+  const value: string
+  export default value
+}
+
+declare module '*.ico' {
+  const value: string
+  export default value
+}
