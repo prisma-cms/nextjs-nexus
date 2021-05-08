1d1cadaf5e2345a6e51a22e24cbc1444f388a698
Fi1osof <n.lanets@freecode.academy>
2021-05-08 14:47:51 +0300

    graphql-shield types
    https://freecode.academy/tasks/ckofm16za40560899twjru3bn

--- server/graphqlServer/permissions/index.ts
+++ server/graphqlServer/permissions/index.ts
@@ -1,14 +1,23 @@
 import { shield } from 'graphql-shield'
+import { Rule } from 'graphql-shield/dist/rules'
+import { NexusGenFieldTypes } from 'server/nexus/generated/nexus'
+// import { isAuthenticated } from './rules/is-authenticated'
 
-// import { isAuthenticated } from "./rules/is-authenticated";
+type RuleTree<K extends NexusGenFieldTypes> = {
+  [P in keyof K]?: RuleTreeRule<K[P]>
+}
 
-// const rules = {
-//   users: isAuthenticated,
-// };
+// https://github.com/microsoft/TypeScript/issues/15300
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type RuleTreeRule<K extends Record<string, any>> = {
+  [P in keyof K]?: Rule
+}
 
-export const permissions = shield({
+const ruleTree: RuleTree<NexusGenFieldTypes> = {
   Query: {
-    // users: rules.users,
+    // me: isAuthenticated,
   },
   Mutation: {},
-})
+}
+
+export const permissions = shield(ruleTree)
