7e797f64485a50e89a57ba8cc7068b98684c21eb
Fi1osof <n.lanets@freecode.academy>
2022-05-19 01:31:56 +0300

    ES2022

--- src/theme/helpers/media-query.ts
+++ src/theme/helpers/media-query.ts
@@ -11,7 +11,7 @@ export type MinWidth = Record<BreakpointsKey, (args: any) => any>
 /**
  * @media(min-width)...
  */
-export const minWidth = Object.keys(theme['breakpoints']).reduce(
+export const minWidth = Object.keys(theme.breakpoints).reduce(
   (accumulator, _label) => {
     const label = _label as BreakpointsKey
     accumulator[label] = (args: (theme: DefaultTheme) => string | string) => {
@@ -27,7 +27,7 @@ export const minWidth = Object.keys(theme['breakpoints']).reduce(
       }
 
       return css`
-        @media (min-width: ${theme['breakpoints'][label]}px) {
+        @media (min-width: ${theme.breakpoints[label]}px) {
           ${cssString};
         }
       `
